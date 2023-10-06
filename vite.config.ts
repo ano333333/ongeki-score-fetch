08b03ddcd55e4055081fd88ccf440a32e6527492
ano3 <172483903+ano333333@users.noreply.github.com>
2023-10-07 04:16:38 +0900

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
-import fs from "fs";
 
 export default defineConfig({
+    root: "src",
     build: {
         rollupOptions: {
             input: {
-                popup: path.join(__dirname, "src/popup.html"),
+                popup: "src/popup.html",
             },
         },
-        outDir: "dist",
+        outDir: "../dist",
         assetsDir: ".",
         emptyOutDir: true,
     },

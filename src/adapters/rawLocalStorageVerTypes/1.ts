dc5a7571bfbb8964f6996c5ee9187c5c0f5df42f
ano333333 <172483903+ano333333@users.noreply.github.com>
2024-10-23 02:10:32 +0900

    fix: LocalStorage生データ形式にoutputTargetを追加

--- src/adapters/rawLocalStorageVerTypes/1.ts
+++ src/adapters/rawLocalStorageVerTypes/1.ts
@@ -5,6 +5,7 @@ export type RawLocalStorageVer1Type = {
 		message: string;
 		type: "progress" | "error" | "finished";
 	}>;
+	outputTarget: "download" | "dropbox";
 	outputTargetOptions: {
 		dropbox: {
 			outputPath: string;
@@ -17,9 +18,10 @@ export type RawLocalStorageVer1Type = {
 export const defaultRawLocalStorageVer1 = {
 	version: 1,
 	progresses: [],
+	outputTarget: "download",
 	outputTargetOptions: {
 		dropbox: {
 			outputPath: "ongeki-score-fetch/data.csv",
 		},
 	},
-};
+} as const;

420f30c6caa1fee0c86231545247e970210313f2
ano333333 <172483903+ano333333@users.noreply.github.com>
2024-11-21 04:37:42 +0900

    fix: sheet-scraperの環境変数の設定をterraformから行う

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -12,3 +12,18 @@ variable "env" {
   description = "prd/stg/(空白)"
   type        = string
 }
+
+variable "sega_user_name" {
+  description = "セガユーザー名(sheet-scraperで使用)"
+  type        = string
+}
+
+variable "sega_password" {
+  description = "セガパスワード(sheet-scraperで使用)"
+  type        = string
+}
+
+variable "spreadsheet_id" {
+  description = "スプレッドシートID(sheet-scraperで使用)"
+  type        = string
+}

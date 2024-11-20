23f60813802c963a786242bf5e4d97ca66c8a866
ano333333 <172483903+ano333333@users.noreply.github.com>
2024-11-20 19:35:09 +0900

    feat: 保存形式をcsvに変更

--- gcp/sheet-scraper/codes/index.ts
+++ gcp/sheet-scraper/codes/index.ts
@@ -2,15 +2,17 @@ import http from "node:http";
 import { getMusicInfoFromOngekiMypage } from "./getMusicInfoFromOngekiMypage";
 import { getSpreadsheetBeatmapInfos } from "./getSpreadsheetBeatmapInfos";
 import { compileReturnResults } from "./compileReturnResults";
+import { dumpReturnResultCsv } from "./dumpReturnResultCsv";
 
 const server = http.createServer(async (req, res) => {
 	console.log(`${req.method} ${req.url}`);
 	const musicInfoList = await getMusicInfoFromOngekiMypage();
 	const spreadsheetBeatmapInfos = await getSpreadsheetBeatmapInfos();
 	const results = compileReturnResults(musicInfoList, spreadsheetBeatmapInfos);
+	const resultString = dumpReturnResultCsv(results);
 	res.statusCode = 200;
-	res.setHeader("Content-Type", "application/json");
-	res.end(JSON.stringify(results));
+	res.setHeader("Content-Type", "text/csv");
+	res.end(resultString);
 });
 
 server.listen(8080, () => {

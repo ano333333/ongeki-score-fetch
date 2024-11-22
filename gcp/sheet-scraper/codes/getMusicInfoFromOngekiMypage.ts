753305668c8ecdabac83d5269adebc0545c134b1
ano333333 <172483903+ano333333@users.noreply.github.com>
2024-11-22 12:37:19 +0900

    feat: オンゲキマイページから、各曲の登場バージョンを抜き出す

--- gcp/sheet-scraper/codes/getMusicInfoFromOngekiMypage.ts
+++ gcp/sheet-scraper/codes/getMusicInfoFromOngekiMypage.ts
@@ -1,11 +1,14 @@
 import type { Page } from "playwright";
 import { openOngekiMypageUrl } from "./openOngekiMypageUrl";
 import { saveOngekiMypageAuth } from "./saveOngekiMypageAuth";
+import { sleep } from "./sleep";
 
 export type OngekiMypageMusicInfo = {
 	title: string;
 	genre: string;
 	character: string;
+	versionMaster: string | null; // BASIC~MASTER譜面の登場バージョン
+	versionLunatic: string | null; // LUNATIC譜面の登場バージョン
 };
 
 const GENRE_MASTER_RECORD_PAGE_URL =
@@ -16,6 +19,21 @@ const CHARACTER_MASTER_RECORD_PAGE_URL =
 	"https://ongeki-net.com/ongeki-mobile/record/musicCharacter/search/?chara=99&diff=3";
 const CHARACTER_LUNATIC_RECORD_PAGE_URL =
 	"https://ongeki-net.com/ongeki-mobile/record/musicCharacter/search/?chara=99&diff=10";
+
+// [バージョン名, バージョンID]の配列
+// 該当バージョンのMASTER/LUNATICのジャンルごと楽曲別レコードページURLは、`https://ongeki-net.com/ongeki-mobile/record/musicScoreGenre/search/?version=${versionId}&diff=${3/10}`
+const VERSION_RECORD_PAGE_URLS: [string, number][] = [
+	["オンゲキ", 1000],
+	["オンゲキ PLUS", 1005],
+	["SUMMER", 1010],
+	["SUMMER PLUS", 1015],
+	["R.E.D.", 1020],
+	["R.E.D. PLUS", 1025],
+	["bright", 1030],
+	["bright MEMORY Act.1", 1035],
+	["bright MEMORY Act.2", 1040],
+	["bright MEMORY Act.3", 1045],
+];
 
 /**
  * オンゲキマイページから取得可能な、各曲の
@@ -35,35 +53,26 @@ export async function getMusicInfoFromOngekiMypage(
 	}
 	await saveOngekiMypageAuth(userName, password, authFilePath);
 
-	// ジャンルごとの曲一覧を取得
-	console.log("getMusicInfoFromOngekiMypage get genre music info start");
+	console.log("getMusicInfoFromOngekiMypage get music info start");
 
-	// 4ページの解析を直列で行うと遅い(レスポンスがtimeoutする)ので、2×2の並列でおこなう
-	// (全て一気にPromise.allするとメモリ不足になる)
-	const threads = [
-		openOngekiMypageUrl(
-			GENRE_MASTER_RECORD_PAGE_URL,
-			getTitlesWithSectionName,
-			authFilePath,
-		),
-		openOngekiMypageUrl(
+	// 1. スタンダードコースの楽曲別レコード一覧から、曲名ごとのジャンル/キャラクターを取得
+	// 4ページの解析を直列で行うと遅い(レスポンスがtimeoutする)ので、並列でおこなう
+	const threads1 = [
+		getTitlesFromStandardRecordPage(GENRE_MASTER_RECORD_PAGE_URL, authFilePath),
+		getTitlesFromStandardRecordPage(
 			GENRE_LUNATIC_RECORD_PAGE_URL,
-			getTitlesWithSectionName,
 			authFilePath,
 		),
-
-		openOngekiMypageUrl(
+		getTitlesFromStandardRecordPage(
 			CHARACTER_MASTER_RECORD_PAGE_URL,
-			getTitlesWithSectionName,
 			authFilePath,
 		),
-		openOngekiMypageUrl(
+		getTitlesFromStandardRecordPage(
 			CHARACTER_LUNATIC_RECORD_PAGE_URL,
-			getTitlesWithSectionName,
 			authFilePath,
 		),
 	];
-	const threadResults = await Promise.all(threads);
+	const threadResults = await Promise.all(threads1);
 
 	const titleGenreMap = new Map<string, string>([
 		...threadResults[0],
@@ -74,27 +83,69 @@ export async function getMusicInfoFromOngekiMypage(
 		...threadResults[2],
 		...threadResults[3],
 	]);
-	// 両方を合わせてOngekiMypageMusicInfoの配列にする
+
+	// 2. プレミアムコースの楽曲別レコード一覧から、MASTER/LUNATICの登場バージョンを取得
+	// 再ログインしないとエラーページに飛びやすい
+	await saveOngekiMypageAuth(userName, password, authFilePath);
+	const titleMasterVersionMap = new Map<string, string>();
+	const titleLunaticVersionMap = new Map<string, string>();
+	for (const [versionName, versionId] of VERSION_RECORD_PAGE_URLS) {
+		const threads2 = [
+			getTitlesFromPremiumRecordPage(
+				`https://ongeki-net.com/ongeki-mobile/record/musicScoreGenre/search/?version=${versionId}&diff=3`,
+				authFilePath,
+			),
+			getTitlesFromPremiumRecordPage(
+				`https://ongeki-net.com/ongeki-mobile/record/musicScoreGenre/search/?version=${versionId}&diff=10`,
+				authFilePath,
+			),
+		];
+		const [threadResultMaster, threadResultLunatic] =
+			await Promise.all(threads2);
+		for (const title of threadResultMaster) {
+			titleMasterVersionMap.set(title, versionName);
+		}
+		for (const title of threadResultLunatic) {
+			titleLunaticVersionMap.set(title, versionName);
+		}
+		await sleep(3000);
+	}
+
+	// 3. 1と2を合わせてOngekiMypageMusicInfoの配列にする
 	const musicInfoList: OngekiMypageMusicInfo[] = [];
 	for (const [title, genre] of titleGenreMap) {
 		const character = titleCharacterMap.get(title) ?? "";
-		musicInfoList.push({ title, genre, character });
+		const versionMaster = titleMasterVersionMap.get(title) ?? null;
+		const versionLunatic = titleLunaticVersionMap.get(title) ?? null;
+		musicInfoList.push({
+			title,
+			genre,
+			character,
+			versionMaster,
+			versionLunatic,
+		});
 	}
 	// "Singularity"の3曲は手動で挿入
 	musicInfoList.push({
 		title: "Singularity -ETIA-",
 		genre: "VARIETY",
 		character: "光",
+		versionMaster: "R.E.D.",
+		versionLunatic: null,
 	});
 	musicInfoList.push({
 		title: "Singularity -MJ-",
 		genre: "VARIETY",
 		character: "九條 楓",
+		versionMaster: "R.E.D. PLUS",
+		versionLunatic: null,
 	});
 	musicInfoList.push({
 		title: "Singularity -technoplanet-",
 		genre: "オンゲキ",
 		character: "井之原 小星",
+		versionMaster: "SUMMER PLUS",
+		versionLunatic: null,
 	});
 
 	console.log("getMusicInfoFromOngekiMypage end");
@@ -102,65 +153,94 @@ export async function getMusicInfoFromOngekiMypage(
 }
 
 /**
- * マイページの曲一覧から、曲タイトルとそれが属するセクション名を取得(ただし"Singularity"は除く)
- * @param page
+ * スタンダードコースの楽曲別レコードページから、曲タイトルとそれが属するセクション名を取得(ただし"Singularity"は除く)
+ * @param url
+ * @param authFilePath
  * @returns 曲タイトルをkey、セクション名をvalueとするMap
  */
-async function getTitlesWithSectionName(page: Page) {
-	const musicListDivSelector =
-		"body > div.wrapper.main_wrapper.t_c > div.container3";
-	const musicListDiv = await page.locator(musicListDivSelector);
-	if (musicListDiv === null) {
-		throw new Error();
-	}
-
-	const allDivsCount = await musicListDiv.locator("> div").count();
-	console.log(`${page.url()} div number: ${allDivsCount}`);
-
-	// タイトルをkey、セクション名をvalueとするMap
-	const sections = new Map<string, string>();
-
-	/** musicListDivの子をパース
-	 * 次のパターンを持つdivを上から順に検索する
-	 * 1. <div class="p_5 f_20">: innerTextがセクション名を表す
-	 * 2. <div class="basic_btn master_score_back m_10 p_5 t_l">: 中のdivのinnerTextが曲タイトルを表す
-	 *  (2.の子孫要素の<div class="music_label p_5 break">のinnerTextが曲タイトルを表す)
-	 */
-	let currentSectionName: string | null = null;
-	let divIndex = 0;
-	for (const child of await musicListDiv.locator("> div").all()) {
-		if (divIndex % 100 === 0) {
-			console.log(`${page.url()} div index: ${divIndex}/${allDivsCount}`);
+async function getTitlesFromStandardRecordPage(
+	url: string,
+	authFilePath: string,
+) {
+	const callback = async (page: Page) => {
+		const musicListDivSelector =
+			"body > div.wrapper.main_wrapper.t_c > div.container3";
+		const musicListDiv = await page.locator(musicListDivSelector);
+		if (musicListDiv === null) {
+			throw new Error();
 		}
-		const tagName = await child.evaluate((el) => el.tagName);
-		if (tagName !== "DIV") {
-			continue;
-		}
-		const classList = (await child.getAttribute("class"))?.split(" ");
-		if (!classList) {
-			continue;
-		}
-		if (
-			classList.findIndex((c) => c === "p_5") !== -1 &&
-			classList.findIndex((c) => c === "f_20") !== -1
-		) {
-			const sectionName = await child.innerText();
-			if (sectionName) {
-				currentSectionName = sectionName;
+
+		const allDivsCount = await musicListDiv.locator("> div").count();
+		console.log(`${page.url()} div number: ${allDivsCount}`);
+
+		// タイトルをkey、セクション名をvalueとするMap
+		const sections = new Map<string, string>();
+
+		/** musicListDivの子をパース
+		 * 次のパターンを持つdivを上から順に検索する
+		 * 1. <div class="p_5 f_20">: innerTextがセクション名を表す
+		 * 2. <div class="basic_btn master_score_back m_10 p_5 t_l">: 中のdivのinnerTextが曲タイトルを表す
+		 *  (2.の子孫要素の<div class="music_label p_5 break">のinnerTextが曲タイトルを表す)
+		 */
+		let currentSectionName: string | null = null;
+		let divIndex = 0;
+		for (const child of await musicListDiv.locator("> div").all()) {
+			if (divIndex % 100 === 0) {
+				console.log(`${page.url()} div index: ${divIndex}/${allDivsCount}`);
 			}
-		} else if (
-			classList.findIndex((c) => c === "basic_btn") !== -1 &&
-			classList.findIndex((c) => c === "m_10") !== -1 &&
-			classList.findIndex((c) => c === "p_5") !== -1 &&
-			classList.findIndex((c) => c === "t_l") !== -1
-		) {
-			const titleDiv = await child.locator("div.music_label");
-			const title = await titleDiv.innerText();
-			if (title && currentSectionName && title !== "Singularity") {
-				sections.set(title, currentSectionName);
+			const tagName = await child.evaluate((el) => el.tagName);
+			if (tagName !== "DIV") {
+				continue;
 			}
+			const classList = (await child.getAttribute("class"))?.split(" ");
+			if (!classList) {
+				continue;
+			}
+			if (
+				classList.findIndex((c) => c === "p_5") !== -1 &&
+				classList.findIndex((c) => c === "f_20") !== -1
+			) {
+				const sectionName = await child.innerText();
+				if (sectionName) {
+					currentSectionName = sectionName;
+				}
+			} else if (
+				classList.findIndex((c) => c === "basic_btn") !== -1 &&
+				classList.findIndex((c) => c === "m_10") !== -1 &&
+				classList.findIndex((c) => c === "p_5") !== -1 &&
+				classList.findIndex((c) => c === "t_l") !== -1
+			) {
+				const titleDiv = await child.locator("div.music_label");
+				const title = await titleDiv.innerText();
+				if (title && currentSectionName && title !== "Singularity") {
+					sections.set(title, currentSectionName);
+				}
+			}
+			divIndex++;
 		}
-		divIndex++;
-	}
-	return sections;
+		return sections;
+	};
+	return openOngekiMypageUrl(url, callback, authFilePath);
+}
+
+/**
+ * プレミアムコースの楽曲別レコードページから、曲タイトルを取得(ただし"Singularity"は除く)
+ * @param url
+ * @param authFilePath
+ * @returns 曲タイトルの配列
+ */
+async function getTitlesFromPremiumRecordPage(
+	url: string,
+	authFilePath: string,
+) {
+	const callback = async (page: Page) => {
+		// music_labelクラスを含むdivのinnerTextがtitleにあたる
+		const xpath = '//div[contains(@class, "music_label")]';
+		const divs = page.locator(xpath);
+		const divsCount = await divs.count();
+		console.log(`${page.url()} div number: ${divsCount}`);
+		const titles = await divs.allInnerTexts();
+		return titles.filter((title) => title !== "Singularity");
+	};
+	return openOngekiMypageUrl(url, callback, authFilePath);
 }

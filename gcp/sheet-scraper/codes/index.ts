a1546dc236d50481b09d42f5932b1d0715249bd8
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-05-26 01:27:31 +0900

    fix: ロジックの結合

--- gcp/sheet-scraper/codes/index.ts
+++ gcp/sheet-scraper/codes/index.ts
@@ -1,40 +1,98 @@
 import http from "node:http";
-import { getMusicInfoFromOngekiMypage } from "./getMusicInfoFromOngekiMypage";
 import { getSpreadsheetBeatmapInfos } from "./getSpreadsheetBeatmapInfos";
-import { compileReturnResults } from "./compileReturnResults";
 import { dumpReturnResultCsv } from "./dumpReturnResultCsv";
 import fs from "node:fs";
-import { uploadToSheetStorage } from "./uploadToBucket";
-import { loadOldCompiledResults } from "./loadOldCompiledResults";
+import { uploadToSheetStorage } from "./utils/uploadToSheetStorage";
 import { removeOngekiMypageAuth } from "./removeOngekiMypageAuth";
+import { getStdRecordPageMusicDatas } from "./logics/getStdRecordPageMusicDatas";
+import { saveOngekiMypageAuth } from "./utils/saveOngekiMypageAuth";
+import { scrapeHtml } from "./utils/scrapeHtml";
+import { downloadFromCloudStorage } from "./utils/downloadFromSheetStorage";
+import { loadResultCsv } from "./logics/loadResultCsv";
+import { overwriteResultCsvRowsMap } from "./logics/overwriteResultCsvRowsMap";
+import { getPrmRecordPageMusicDatas } from "./logics/getPrmRecordPageMusicDatas";
+import { createResultCsvRowsMap } from "./logics/createResultCsvRowsMap";
+import path from "node:path";
+
+const authFilePath = path.resolve(__dirname, "../auth.json");
+const localCsvPath = path.resolve(__dirname, "./result.csv");
 
 const server = http.createServer(async (req, res) => {
 	try {
 		console.log(`${req.method} ${req.url}`);
-		const musicInfoList = await getMusicInfoFromOngekiMypage();
-		const spreadsheetBeatmapInfos = await getSpreadsheetBeatmapInfos();
-		const newCompiledResults = compileReturnResults(
-			musicInfoList,
-			spreadsheetBeatmapInfos,
+
+		const userName = process.env.SEGA_USER_NAME;
+		const password = process.env.SEGA_PASSWORD;
+		if (!userName || !password) {
+			throw new Error();
+		}
+		await saveOngekiMypageAuth(userName, password, authFilePath);
+
+		// 1. スタンダードページの情報取得
+		const stdDatas = await getStdRecordPageMusicDatas((url) =>
+			scrapeHtml(url, authFilePath),
 		);
 
-		const oldCompiledResults = await loadOldCompiledResults("result.csv");
-		if (
-			JSON.stringify(newCompiledResults) !== JSON.stringify(oldCompiledResults)
-		) {
-			const resultString = dumpReturnResultCsv(newCompiledResults);
-			fs.writeFileSync("result.csv", resultString);
-			await uploadToSheetStorage("result.csv", "result.csv");
+		// 2. スプレッドシートの情報取得
+		const spreadsheetDatas = await getSpreadsheetBeatmapInfos();
+
+		// 3. クラウドストレージからファイルダウンロード
+		const doesCsvExists = await downloadFromCloudStorage(
+			"result.csv",
+			localCsvPath,
+		);
+
+		// 4. 古いデータが存在すればマージし、更新が必要ならば上書き
+		if (doesCsvExists) {
+			console.log("old csv exists");
+			const curVersionName = process.env.CURRENT_ONGEKI_VERSION_NAME;
+			if (!curVersionName) {
+				throw new Error();
+			}
+			const csvDatas = await loadResultCsv(localCsvPath);
+			const needsUpdate = overwriteResultCsvRowsMap(
+				csvDatas,
+				stdDatas.master,
+				stdDatas.lunatic,
+				curVersionName,
+				spreadsheetDatas,
+			);
+			if (needsUpdate) {
+				console.log("needs update");
+				const csvDump = dumpReturnResultCsv(csvDatas);
+				fs.writeFileSync(localCsvPath, csvDump);
+				await uploadToSheetStorage(localCsvPath, "result.csv");
+			} else {
+				console.log("no update");
+			}
+		}
+		// 5. 古いデータが存在しなければ、プレミアムのページにもアクセスし新しいデータを生成
+		else {
+			console.log("old csv not exists");
+			const prmDatas = await getPrmRecordPageMusicDatas((url) =>
+				scrapeHtml(url, authFilePath),
+			);
+			const csvDatas = await createResultCsvRowsMap(
+				stdDatas.master,
+				stdDatas.lunatic,
+				prmDatas.master,
+				prmDatas.lunatic,
+				spreadsheetDatas,
+			);
+			const csvDump = dumpReturnResultCsv(csvDatas);
+			fs.writeFileSync(localCsvPath, csvDump);
+			await uploadToSheetStorage(localCsvPath, "result.csv");
 		}
 	} catch (e) {
 		console.error(e);
 	} finally {
-		if (fs.existsSync("result.csv")) {
-			fs.rmSync("result.csv");
+		if (fs.existsSync(localCsvPath)) {
+			fs.rmSync(localCsvPath);
 		}
 		removeOngekiMypageAuth();
 		res.statusCode = 200;
 		res.end();
+		console.log("end");
 	}
 });
 

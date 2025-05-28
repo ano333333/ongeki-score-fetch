397209643cfe9dd93ab0d568da57460d87ad167c
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-05-29 01:33:44 +0900

    test: testコマンドをscriptsに追加

--- chrome-extension/vite.config.ts
+++ chrome-extension/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -25,5 +26,8 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		open: true,
+	},
+	test: {
+		include: ["../tests/**/*.test.ts"],
 	},
 });
